08540615dc37ba2f386f3786ca23cd3546a3bd85
Mikhail <seninmihail@gmail.com>
2022-09-18 17:14:30 +0500

    added comment to component

--- itkamasutra/src/components/Profile/MyPosts/MyPosts.jsx
+++ itkamasutra/src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import style from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
+// компонент моих постов
 const MyPosts = (props) => {
-  let postsElements = props.posts.map((p) => (
+  const postsElements = props.posts.map((p) => (
     <Post message={p.message} likesCount={p.likesCount} />
   ));
 
