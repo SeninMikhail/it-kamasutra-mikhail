83d7b0454f5e99db9776617ba094502cb4a2d8b7
Mikhail <seninmihail@gmail.com>
2022-11-20 18:41:21 +0500

    урок 60

--- itkamasutra/src/App.js
+++ itkamasutra/src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
 import Nav from "./components/Nav/Nav";
 import Header from "./components/Header/Header";
-import Profile from "./components/Profile/Profile";
-import Dialogs from "./components/Dialogs/Dialogs";
+
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import Users from "./components/Users/Users";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 
@@ -23,7 +21,11 @@ const App = () => {
         <Nav />
         <div className="app-wrapper-content">
           <Routes>
-            <Route path="/profile/*" element={<ProfileContainer />} />
+            {/* <Route path="/profile/:userId" element={<ProfileContainer />} />
+            <Route path="/profile/*" element={<ProfileContainer />} /> */}
+            <Route path="/profile" element={<ProfileContainer />}>
+              <Route path=":userId" element={<ProfileContainer />} />
+            </Route>
             <Route path="/dialogs/*" element={<DialogsContainer />} />
             <Route path="/news/*" element={<News />} />
             <Route path="/music/*" element={<Music />} />
