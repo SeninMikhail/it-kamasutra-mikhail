0acc0d971331d6908a6589b4b3ca111f3abb1123
Mikhail <seninmihail@gmail.com>
2022-11-30 22:21:20 +0500

    урок 71

--- itkamasutra/src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ itkamasutra/src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Preloader from "../../Common/Preloader/Preloader";
 import style from "./ProfileInfo.module.css";
+import ProfileStatus from "./ProfileStatus";
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
@@ -9,13 +10,13 @@ const ProfileInfo = (props) => {
 
   return (
     <div className={style.content}>
-      <div className={style.img}>
+      {/* <div className={style.img}>
         <img src="https://bipbap.ru/wp-content/uploads/2017/04/0_7c779_5df17311_orig.jpg" />
-      </div>
+      </div> */}
 
       <div className={style.descriptionBlock}>
         <img src={props.profile.photos.large} />
-        ava+desc
+        <ProfileStatus status={"Hello"} />
       </div>
     </div>
   );
